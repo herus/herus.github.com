50e97d4c55a4da7521dad8c6d32e39e6f376cd12
Heru Setiawan <heru.openid@gmail.com>
2017-08-08 19:39:17 +0900

    nanogp

---
+++ nanogp/admin/config.php
@@ -0,0 +1,7 @@
+<?php
+$cfg_client_id          = '688495482721-f6e3b2j1t2d5sue4ebne04pb89ljmra4.apps.googleusercontent.com';
+$cfg_client_secret      = '1db2HY4F0HviAUqoYOFUofhq';
+$cfg_max_accounts       = 1;
+$cfg_log                = false;
+$albums_filter          = ['sauvegarde', 'backup'];
+?>
